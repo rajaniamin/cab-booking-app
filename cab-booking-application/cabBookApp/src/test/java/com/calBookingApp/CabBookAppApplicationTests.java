4fc7da49c94bfb32e8e27284e7bc585139b7d19a
Rajani VC <112753940+rajaniamin@users.noreply.github.com>
2023-06-29 12:26:07 +0530

    created packages

---
+++ cab-booking-application/cabBookApp/src/test/java/com/calBookingApp/CabBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
